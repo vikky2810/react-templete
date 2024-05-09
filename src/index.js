f63c35ac2a26d33e7d807a08f311dcca3befaa0b
Engineervikky <107768078+vikky2810@users.noreply.github.com>
2024-05-10 00:47:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,40 @@ root.render(
   </React.StrictMode>
 );
 
+const express = require('express')
+const expressApp = express()
+const axios = require("axios");
+const path = require("path")
+const port = process.env.PORT || 3000;
+expressApp.use(express.static('static'))
+expressApp.use(express.json());
+require('dotenv').config();
+const { Telegraf } = require('telegraf');
+const bot = new Telegraf(process.env.BOT_TOKEN);
+expressApp.get("/", (req, res) => {
+res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+bot.command('start', ctx => {
+    console.log(ctx.from)
+    bot.telegram.sendMessage(ctx.chat.id,
+         'I am Vikram . Welcome to the best bot ever create by me.\nI respond to /ethereum. Please try it', {
+    })
+    })
+    bot.command('ethereum', ctx => {
+    var rate;
+    console.log(ctx.from)
+    axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd`)
+    .then(response => {
+    console.log(response.data)
+    rate = response.data.ethereum
+    const message = `Hello, today the ethereum price is ${rate.usd}USD`
+    bot.telegram.sendMessage(ctx.chat.id, message, {
+    })
+    })
+    })
+bot.launch()
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
